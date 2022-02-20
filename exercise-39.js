3692d9032f132f608a3abb05b324ea742a485ed5
Leonarch <arcomat@virgilio.it>
2022-02-20 17:30:00 +0100

    Update1

---
+++ exercise-39.js
@@ -0,0 +1,42 @@
+class Square {
+    constructor(side) {
+      this.side = side;
+    }
+  }
+  
+  class Rectangle {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+  
+  class Circle {
+    constructor(radius) {
+      this.radius = radius;
+      
+    }
+  }
+  
+  class AreaCalculator {
+  
+      static calculate (area) {
+        
+          if (area instanceof Square){
+          return square.side**2;
+          }
+          if (area instanceof Rectangle){
+            return rectangle.width * rectangle.height;
+         }
+        if (area instanceof Circle){
+            return Math.PI*circle.radius**2;
+       }
+  }}
+
+  const square = new Square(4);
+  const rectangle = new Rectangle(4, 2);
+  const circle = new Circle(5);
+  
+  console.log(AreaCalculator.calculate(square));
+  console.log(AreaCalculator.calculate(rectangle));
+  console.log(AreaCalculator.calculate(circle));
